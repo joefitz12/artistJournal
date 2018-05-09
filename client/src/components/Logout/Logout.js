0d836a2e95e2b4984654c1467b3955b1d757c595
Joe Fitzpatrick <joe@Joes-MacBook-Pro.local>
2018-05-09 15:46:13 -0500

    updates logout font

--- client/src/components/Logout/Logout.js
+++ client/src/components/Logout/Logout.js
@@ -6,7 +6,7 @@ import "./Logout.css";
 
 const Logout = props => (
   <span id="logout-div">
-    {props.isAuthorized && <FormBtn id="logout-btn" onClick={props.handleLogout}>Logout</FormBtn> }
+    {props.isAuthorized && <FormBtn id="logout-btn" onClick={props.handleLogout}>logout</FormBtn> }
   </span>
 );
 
